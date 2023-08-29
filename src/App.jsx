59b09d718fc2368b8a4b47f70ee5f22124be7b90
Irfan Ahmad <irfanft12@gmail.com>
2023-08-29 16:46:46 +0530

    improved delet message

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ export default function App() {
 
     MySwal.fire({
       title: "Are you sure?",
-      text: "You won't be able to revert this!",
+      text: `You won't be able to revert ${item.todo}!`,
       icon: "warning",
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
